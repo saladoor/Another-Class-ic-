8f164f72913965853a1f3bbde9ef7e8116a87277
AugustM94 <s144461@student.dtu.dk>
2016-11-06 20:43:34 +0100

    Adds model folder plus file.

---
+++ WpfApplication1/WpfApplication1/Model/ClassBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Model
+{
+    using System;
+
+    public class ClassBox
+    {
+        private String className;
+        private int xPos, yPos;
+        private String[] feilds;
+        private String[] methods;
+
+
+    }
+}
