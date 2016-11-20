434e1fec2d0aa60e4880ace19d4b29fb374d776c
August Møbius <am_94@live.dk>
2016-11-20 15:43:12 +0100

    Adds drawing class

---
+++ WpfApplication1/WpfApplication1/Model/View/DrawItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.View
+{
+    class DrawItem
+    {
+        public void drawClassBox(ClassBox box)
+        {
+
+        }
+        
+    }
+}
