ea79cb39798925c32ba5bd75d61a9990190f37a3
marenMKG <marenMKG@MARENGUNNARA864>
2016-11-29 08:36:19 +0100

    Images on buttons

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace WpfApplication1
         {
             InitializeComponent();
         }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            throw new NotImplementedException();
+        }
     }
 }
