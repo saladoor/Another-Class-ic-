3a07bb32166ccb55d62e0f7194f6295625c2f897
marenMKG <marenMKG@MARENGUNNARA864>
2016-11-30 00:26:13 +0100

    ToolbarView almost finished

--- WpfApplication1/WpfApplication1/View/ToolbarView.xaml.cs
+++ WpfApplication1/WpfApplication1/View/ToolbarView.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace WpfApplication1.View
 {
     /// <summary>
-    /// Interaction logic for Page1.xaml
+    /// Interaction logic for ToolbarView.xaml
     /// </summary>
-    public partial class Page1 : Page
+    public partial class ToolbarView : Page
     {
-        public Page1()
+        public ToolbarView()
         {
             InitializeComponent();
         }
