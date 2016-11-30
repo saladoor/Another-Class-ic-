dba580f468aec122afbfd902a9a432379c5ed603
marenMKG <marenMKG@MARENGUNNARA864>
2016-11-30 11:28:23 +0100

    MainView

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -29,10 +29,6 @@ namespace WpfApplication1
         }
 
 
-        private void Menu_Click(object sender, RoutedEventArgs e)
-        {
 
-            throw new NotImplementedException();
-        }
     }
 }
